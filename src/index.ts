132ee149ae29c663099e079b149884d9d956e765
Puwarun Puncreobutr <puwarun@thaibma.or.th>
2022-08-08 13:46:11 +0700

    Init

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import http from "http";
+import express, { NextFunction, Request, Response } from "express";
+
+export interface ResponseError {
+  message: string;
+  stacktrace: string;
+}
+
+export interface ResponseMessage {
+  statusCode?: number;
+  responseCode?: number;
+  message: string;
+  errors?: ResponseError[];
+}
+
+type StatusCodes = 200 | 201 | 400 | 401 | 403 | 405 | 500 | 502;
+
+declare global {
+  namespace Express {
+    interface Response {
+        respond(resultCode: number, msg: string): any;      
+        respond(resultCode: string, msg: string): any;
+        respond(resultCode: number, msg: string, data: any): any;
+        respond(resultCode: string, msg: string, data: any): any;
+        
+        reject(errors: ResponseError[]): any;
+    }
+  }
+
+  namespace http {
+    interface ServerResponse {
+      respond(): any;
+    }
+  }
+}
+
+export function respond(
+  response: http.ServerResponse,
+  message: ResponseMessage
+) {
+  response.setHeader("content-type", "application/json");
+  if (message.errors && message.errors.length > 0) {
+    response.statusCode = message.statusCode || 500;
+  }
+
+  if (message.statusCode) {
+    delete message.statusCode;
+  }
+
+  response.end(JSON.stringify(message));
+}
+
+export const expressMiddleware = () => (
+  req: Request,
+  res: Response,
+  next: any
+): void => {
+  res.respond = (
+    resultCode: number | string,
+    msg: string,
+    data?: any
+  ): void => {
+    let responseMessage = {
+      code: resultCode,
+      msg,
+      data,
+    };
+
+    if (!data) {
+      delete responseMessage.data;
+    }
+
+    res.send(responseMessage);
+    return;
+  };
+
+  res.reject = (errors: ResponseError[], statusCode?: number) => {
+    res.status(502);
+    res.send({
+      data: null,
+      errors,
+    });
+    return;
+  };
+
+  next && next();
+};
